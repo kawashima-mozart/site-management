94c3a44fbe13d0f1bc00a9dcdf9e358998ca7e9b
kawashima-mozart <nowhere.no.where.now.here.orz@gmail.com>
2020-11-24 21:16:42 +0900

    モデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true
+  with_options presence:true do 
+    validates :name
+    validates :password_confirmation
+  end
 end

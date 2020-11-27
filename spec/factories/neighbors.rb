e999118a7ab5163f774024c5bc55052a9eb002bf
kawashima-mozart <nowhere.no.where.now.here.orz@gmail.com>
2020-11-27 17:02:12 +0900

    neighborのテストコード

--- spec/factories/neighbors.rb
+++ spec/factories/neighbors.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :neighbor do
-    
+    lot_number {'東1-1-1'}
+    name { '佐藤 太郎'}
+    address { '東一丁目1番１' }
+    address_now { '南一丁目１番１' }
+    phone_number { 123-1234-1234 }
+    witness { 1 }
+    witness_day { 2020-01-01 }
+    imprint { 1 }
+    imprint_day { 2020-01-01 }
+    status_id { 1 }
+    memo { 'メモメモメモ' }
+
+    association :site
   end
 end
--- spec/models/neighbor_spec.rb
+++ spec/models/neighbor_spec.rb
@@ -1,5 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe Neighbor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @neighbor = FactoryBot.build(:neighbor)
+  end
+
+  describe '近隣情報登録' do
+    context '保存できるとき' do
+      it '正しい値が入力されているとき' do
+        expect(@neighbor).to be_valid
+      end
+      it '現住所が空欄でも保存できる' do
+        @neighbor.address_now = nil
+        expect(@neighbor).to be_valid
+      end
+      it '電話番号が空でも保存できる' do
+        @neighbor.phone_number = nil
+        expect(@neighbor).to be_valid
+      end
+      end
+      it '状況を選択していなくても保存できる' do
+        @neighbor.status_id = 0
+        expect(@neighbor).to be_valid
+      end
+      it '立会のチェックがついていなくても保存できる' do
+        @neighbor.witness = 0
+        expect(@neighbor).to be_valid
+      end
+      it '立会日時が空でも保存できる' do
+        @neighbor.witness_day = nil
+        expect(@neighbor).to be_valid
+      end
+      it '押印のチェックがついていなくても保存できる' do
+        @neighbor.imprint = 0
+        expect(@neighbor).to be_valid
+      end
+      it '押印日時が空でも保存できる' do
+        @neighbor.imprint_day = nil
+        expect(@neighbor).to be_valid
+      end
+      it 'メモが空でも保存できる' do
+        @neighbor.memo = nil
+        expect(@neighbor).to be_valid
+      end
+    end
+
+    context '保存できないとき' do
+      it '地番が空欄のとき' do
+        @neighbor.lot_number = nil
+        @neighbor.valid?
+        expect(@neighbor.errors.full_messages).to include('地番を入力してください')      
+      end
+      it '名前が空欄のとき' do
+        @neighbor.name = nil
+        @neighbor.valid?
+        expect(@neighbor.errors.full_messages).to include('名前を入力してください')      
+      end
+      it '登記住所が空欄のとき' do
+        @neighbor.address = nil
+        @neighbor.valid?
+        expect(@neighbor.errors.full_messages).to include('登記簿住所を入力してください')      
+      end
+      it '地番が空欄のとき' do
+        @neighbor.lot_number = nil
+        @neighbor.valid?
+        expect(@neighbor.errors.full_messages).to include('地番を入力してください')      
+      end
+    end
+  end
 end

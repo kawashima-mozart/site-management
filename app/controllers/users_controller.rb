1cb5da1673f6faaa58bba66a66641f6579307c4d
kawashima-mozart <nowhere.no.where.now.here.orz@gmail.com>
2020-11-30 01:08:18 +0900

    カレンダー追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id]) 
+    @schedules = @user.schedules.includes(:user)
+    @schedule = Schedule.new
   end 
 end

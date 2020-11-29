14c94b16255d445e51150d4f441fb06ac7c2b8d7
kawashima-mozart <nowhere.no.where.now.here.orz@gmail.com>
2020-11-29 18:43:31 +0900

    コメント登録機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.create(comment_params)
+    redirect_to edit_site_business_path(params[:site_id],params[:business_id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, business_id: params[:business_id])
+  end
 end

ed65c3794b4e324c5e991d240e91733e975a2ccf
kawashima-mozart <nowhere.no.where.now.here.orz@gmail.com>
2020-12-02 13:46:46 +0900

    イベントモデル作成

--- db/migrate/20201202032815_create_events.rb
+++ db/migrate/20201202032815_create_events.rb
@@ -1,6 +1,9 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
+      t.string :title, null: false
+      t.date :start, null: false
+      t.date :end
 
       t.timestamps
     end

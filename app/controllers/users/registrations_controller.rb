7d3cb4e78acd400755c054bdabcaa811b97b3c53
kawashima-mozart <nowhere.no.where.now.here.orz@gmail.com>
2020-12-07 18:20:36 +0900

    ゲストユーザー削除防止

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :check_guest, only: :destroy
+
+  def check_guest
+    if resource.email == 'guest@example.com'
+      redirect_to root_path
+    end
+  end
+
+end
